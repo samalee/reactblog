ebc52a0637f95237a23b7e130f254d61b0a36d98
Samantha Malay <samaleemalay@gmail.com>
2019-09-25 18:17:31 -0600

    react-blog

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,34 @@
+class Api::PostsController < ApplicationController
+  def index 
+    render json: Post.all
+  end
+
+  def create 
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post
+    else
+      render json: { errors: @post.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: { errors: @post.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    render json: { message: 'post deleted'}
+  end
+
+  private
+    def post_params
+      # { post: {title: '', body: ''} }
+      params.require(:post).permit(:title, :body)
+    end
+end
